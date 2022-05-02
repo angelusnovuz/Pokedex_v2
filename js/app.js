4e9e42c4973f8c061602bfb4c6caf9a7c4fe648b
Angel Lopez Hernandez <73920367+angelusnovuz@users.noreply.github.com>
2022-05-02 15:50:53 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,94 @@
+// FUNCIONES DE LLAMADO
+
+const pokeCard = document.querySelector('[pokeContainer]');
+const pokeName = document.querySelector('[pokeNombre]');
+const pokeImg = document.querySelector('[pokeImg]');
+const pokeImgContainer = document.querySelector('[pokeImgContainer]');
+const pokeId = document.querySelector('[pokeNumero]');
+const pokeTypes = document.querySelector('[pokeTipo]');
+const pokeStats = document.querySelector('[pokeEstadisticas]');
+
+// COLORES POR CADA TIPO DE POKEMON
+
+const typeColors = {
+    electric: '#FFEA70',
+    normal: '#B09398',
+    fire: '#FF675C',
+    water: '#0596C7',
+    ice: '#AFEAFD',
+    rock: '#999799',
+    flying: '#7AE7C7',
+    grass: '#4A9681',
+    psychic: '#FFC6D9',
+    ghost: '#561D25',
+    bug: '#A2FAA3',
+    poison: '#795663',
+    ground: '#D2B074',
+    dragon: '#DA627D',
+    steel: '#1D8A99',
+    fighting: '#2F2F2F',
+    default: '#2A1A1F',
+};
+
+
+// BUSCAR POKEMON
+
+const buscarPokemon = event => {
+    event.preventDefault();
+    const { value } = event.target.pokemon; // Guardar el input en la variable value
+    fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`) // Concatenar el 'value' con la url de la API
+        .then(data => data.json())
+        .then(response => renderPokemonData(response))
+        .catch(err => renderNotFound())
+}
+
+const renderPokemonData = data => {
+    const sprite =  data.sprites.other.home.front_default;
+    const { stats, types } = data;
+
+    pokeName.textContent = data.name;
+    pokeImg.setAttribute('src', sprite);
+    pokeId.textContent = `Nº ${data.id}`;
+    setCardColor(types);
+    renderPokemonTypes(types);
+    renderPokemonStats(stats);
+}
+
+
+const setCardColor = types => {
+    const colorOne = typeColors[types[0].type.name];
+    const colorTwo = types[1] ? typeColors[types[1].type.name] : typeColors.default;
+}
+
+const renderPokemonTypes = types => {
+    pokeTypes.innerHTML = '';
+    types.forEach(type => {
+        const typeTextElement = document.createElement("div");
+        typeTextElement.style.color = typeColors[type.type.name];
+        typeTextElement.textContent = type.type.name;
+        pokeTypes.appendChild(typeTextElement);
+    });
+}
+
+const renderPokemonStats = stats => {
+    pokeStats.innerHTML = '';
+    stats.forEach(stat => {
+        const statElement = document.createElement("div");
+        const statElementName = document.createElement("div");
+        const statElementAmount = document.createElement("div");
+        statElementName.textContent = stat.stat.name;
+        statElementAmount.textContent = stat.base_stat;
+        statElement.appendChild(statElementName);
+        statElement.appendChild(statElementAmount);
+        pokeStats.appendChild(statElement);
+    });
+}
+
+const renderNotFound = () => {
+    pokeName.textContent = 'No encontrado';
+    pokeImg.setAttribute('src', 'poke-shadow.png');
+    pokeImg.style.background =  '#fff';
+    pokeTypes.innerHTML = '';
+    pokeStats.innerHTML = '';
+    pokeId.textContent = '';
+}
